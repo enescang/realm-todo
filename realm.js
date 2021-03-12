126f01b23767d45ca8d166013db7151e7858fe88
kodcanlisi <developerindiee@gmail.com>
2021-03-12 15:29:35 +0300

    Realm actions

---
+++ realm.js
@@ -0,0 +1,58 @@
+import Realm from 'realm';
+
+const TODO_SCHEMA = 'Todo'
+
+const TodoSchema = {
+    name: TODO_SCHEMA,
+    primaryKey: '_id',
+    properties: {
+        _id: 'string',
+        todo: 'string'
+    }
+}
+const realm = new Realm({ schema: [TodoSchema], schemaVersion: 2 });
+
+const addTodo = (todo) => {
+    try {
+        todo._id = Math.floor(Math.random() * 100000);
+        realm.write(() => {
+            realm.create(TODO_SCHEMA, todo)
+        });
+    } catch (e) {
+        console.log(e);
+    }
+ 
+    console.log(getAllTodos());
+}
+
+const deleteTodo = (_id, content) =>{
+    try {
+        realm.write(()=>{
+            const todo = realm.objectForPrimaryKey(_id);
+            todo.content = content;
+        })
+    } catch (e) {
+        console.log('>>> Delete Error: ', e, ' <<<');
+    }
+}
+
+const clearAll = () => {
+    realm.write(()=>{
+        realm.deleteAll();
+    })
+}
+
+const getAllTodos = () => {
+    const todos = realm.objects(TODO_SCHEMA);
+    return todos;
+}
+
+
+export {
+    TODO_SCHEMA,
+    addTodo,
+    deleteTodo,
+    clearAll,
+    getAllTodos,
+}
+export default realm;
